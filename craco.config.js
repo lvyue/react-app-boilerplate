8982fdfc37c91ec08e36d972f99b2cc94479e8b6
lvyue1 <lvyue1@jd.com>
2021-05-12 14:21:44 +0800

    dev:add eslint,prettier support

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+// const { when, whenDev } = require("@craco/craco");
+
+module.exports = {
+    // eslint: {
+    //     mode: ESLINT_MODES.file,
+    //     configure: {
+    //         formatter: when(process.env.NODE_ENV === "CI", require("eslint-formatter-vso"))
+    //     }
+    // },
+    // webpack: {
+    //     plugins: [
+    //         new ConfigWebpackPlugin(),
+    //         ...whenDev(() => [new CircularDependencyPlugin()], [])
+    //     ]
+    // }
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+
+export {};
+
+declare global {
+    // define new props on window
+    interface Window {
+        CUSTOME_PROP: unknown;
+    }
+    namespace React {
+        interface DOMAttributes<T> {
+            clstag?: string;
+        }
+    }
+}
